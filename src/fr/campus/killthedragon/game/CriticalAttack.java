dd08449e8e2464ee0d22f8ffeae71bef5276da03
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-09-08 15:21:44 +0200

    Suppression d'un print

--- src/fr/campus/killthedragon/game/CriticalAttack.java
+++ src/fr/campus/killthedragon/game/CriticalAttack.java
@@ -12,7 +12,6 @@ public class CriticalAttack {
 
     public void getCritical(int diceFaces) throws CriticalAttackException, CriticalFailureException {
         int criticalStrike = diceCritical.roll(diceFaces);
-        System.out.println(criticalStrike);
         if(criticalStrike==1){
             throw new CriticalFailureException("It's a critical failure!");
         }else if (criticalStrike==diceFaces) {
